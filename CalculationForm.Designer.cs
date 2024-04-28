a2dd05abb47315bacdab0317938df2317d55dd3a
Martin <martingrahovski2004@gmail.com>
2024-04-28 16:00:34 +0300

    Adding LINQ to calculation form

--- CalculationForm.Designer.cs
+++ CalculationForm.Designer.cs
@@ -29,6 +29,16 @@
         private void InitializeComponent()
         {
             lblName = new Label();
+            lblSameFillColor = new Label();
+            lblCountSameFillColor = new Label();
+            lblCountSameOutlineColor = new Label();
+            lblSameOutlineColor = new Label();
+            lblCountAllFigures = new Label();
+            lblAllFigures = new Label();
+            btnChooseColor = new Button();
+            lblColor = new Label();
+            btnCalculate = new Button();
+            btnOK = new Button();
             SuspendLayout();
             // 
             // lblName
@@ -36,22 +46,128 @@
             lblName.AutoSize = true;
             lblName.Font = new Font("Segoe Script", 26.25F, FontStyle.Regular, GraphicsUnit.Point);
             lblName.ImeMode = ImeMode.NoControl;
-            lblName.Location = new Point(179, 45);
+            lblName.Location = new Point(71, 44);
             lblName.Margin = new Padding(6, 0, 6, 0);
             lblName.Name = "lblName";
             lblName.Size = new Size(974, 111);
             lblName.TabIndex = 4;
             lblName.Text = "Calculation of all figures";
             // 
+            // lblSameFillColor
+            // 
+            lblSameFillColor.Font = new Font("Verdana", 13.875F, FontStyle.Regular, GraphicsUnit.Point);
+            lblSameFillColor.Location = new Point(40, 219);
+            lblSameFillColor.Margin = new Padding(4, 0, 4, 0);
+            lblSameFillColor.Name = "lblSameFillColor";
+            lblSameFillColor.Size = new Size(400, 97);
+            lblSameFillColor.TabIndex = 5;
+            lblSameFillColor.Text = "Count of figures with same fill color:";
+            // 
+            // lblCountSameFillColor
+            // 
+            lblCountSameFillColor.Font = new Font("Verdana", 13.875F, FontStyle.Regular, GraphicsUnit.Point);
+            lblCountSameFillColor.Location = new Point(565, 219);
+            lblCountSameFillColor.Margin = new Padding(4, 0, 4, 0);
+            lblCountSameFillColor.Name = "lblCountSameFillColor";
+            lblCountSameFillColor.Size = new Size(161, 97);
+            lblCountSameFillColor.TabIndex = 6;
+            // 
+            // lblCountSameOutlineColor
+            // 
+            lblCountSameOutlineColor.Font = new Font("Verdana", 13.875F, FontStyle.Regular, GraphicsUnit.Point);
+            lblCountSameOutlineColor.Location = new Point(565, 383);
+            lblCountSameOutlineColor.Margin = new Padding(4, 0, 4, 0);
+            lblCountSameOutlineColor.Name = "lblCountSameOutlineColor";
+            lblCountSameOutlineColor.Size = new Size(161, 97);
+            lblCountSameOutlineColor.TabIndex = 8;
+            // 
+            // lblSameOutlineColor
+            // 
+            lblSameOutlineColor.Font = new Font("Verdana", 13.875F, FontStyle.Regular, GraphicsUnit.Point);
+            lblSameOutlineColor.Location = new Point(40, 383);
+            lblSameOutlineColor.Margin = new Padding(4, 0, 4, 0);
+            lblSameOutlineColor.Name = "lblSameOutlineColor";
+            lblSameOutlineColor.Size = new Size(425, 97);
+            lblSameOutlineColor.TabIndex = 7;
+            lblSameOutlineColor.Text = "Count of figures with same outline color:";
+            // 
+            // lblCountAllFigures
+            // 
+            lblCountAllFigures.Font = new Font("Verdana", 13.875F, FontStyle.Regular, GraphicsUnit.Point);
+            lblCountAllFigures.Location = new Point(565, 537);
+            lblCountAllFigures.Margin = new Padding(4, 0, 4, 0);
+            lblCountAllFigures.Name = "lblCountAllFigures";
+            lblCountAllFigures.Size = new Size(161, 97);
+            lblCountAllFigures.TabIndex = 10;
+            // 
+            // lblAllFigures
+            // 
+            lblAllFigures.Font = new Font("Verdana", 13.875F, FontStyle.Regular, GraphicsUnit.Point);
+            lblAllFigures.Location = new Point(40, 537);
+            lblAllFigures.Margin = new Padding(4, 0, 4, 0);
+            lblAllFigures.Name = "lblAllFigures";
+            lblAllFigures.Size = new Size(400, 97);
+            lblAllFigures.TabIndex = 9;
+            lblAllFigures.Text = "Count of all figures:";
+            // 
+            // btnChooseColor
+            // 
+            btnChooseColor.BackgroundImage = Properties.Resources.pickColorLogo;
+            btnChooseColor.BackgroundImageLayout = ImageLayout.Stretch;
+            btnChooseColor.Location = new Point(1338, 44);
+            btnChooseColor.Name = "btnChooseColor";
+            btnChooseColor.Size = new Size(120, 110);
+            btnChooseColor.TabIndex = 11;
+            btnChooseColor.UseVisualStyleBackColor = true;
+            btnChooseColor.Click += btnChooseColor_Click;
+            // 
+            // lblColor
+            // 
+            lblColor.Location = new Point(1477, 44);
+            lblColor.Name = "lblColor";
+            lblColor.Size = new Size(120, 110);
+            lblColor.TabIndex = 12;
+            // 
+            // btnCalculate
+            // 
+            btnCalculate.Location = new Point(565, 1083);
+            btnCalculate.Name = "btnCalculate";
+            btnCalculate.Size = new Size(236, 105);
+            btnCalculate.TabIndex = 13;
+            btnCalculate.Text = "Calculate";
+            btnCalculate.UseVisualStyleBackColor = true;
+            btnCalculate.Click += btnCalculate_Click;
+            // 
+            // btnOK
+            // 
+            btnOK.Location = new Point(851, 1083);
+            btnOK.Name = "btnOK";
+            btnOK.Size = new Size(236, 105);
+            btnOK.TabIndex = 14;
+            btnOK.Text = "OK";
+            btnOK.UseVisualStyleBackColor = true;
+            btnOK.Click += btnOK_Click;
+            // 
             // CalculationForm
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.SpringGreen;
-            ClientSize = new Size(1355, 1217);
+            ClientSize = new Size(1609, 1217);
+            Controls.Add(btnOK);
+            Controls.Add(btnCalculate);
+            Controls.Add(lblColor);
+            Controls.Add(btnChooseColor);
+            Controls.Add(lblCountAllFigures);
+            Controls.Add(lblAllFigures);
+            Controls.Add(lblCountSameOutlineColor);
+            Controls.Add(lblSameOutlineColor);
+            Controls.Add(lblCountSameFillColor);
+            Controls.Add(lblSameFillColor);
             Controls.Add(lblName);
             Name = "CalculationForm";
             Text = "Calculation";
+            Load += CalculationForm_Load;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -59,5 +175,15 @@
         #endregion
 
         private Label lblName;
+        private Label lblSameFillColor;
+        private Label lblCountSameFillColor;
+        private Label lblCountSameOutlineColor;
+        private Label lblSameOutlineColor;
+        private Label lblCountAllFigures;
+        private Label lblAllFigures;
+        private Button btnChooseColor;
+        private Label lblColor;
+        private Button btnCalculate;
+        private Button btnOK;
     }
 }
--- CalculationForm.cs
+++ CalculationForm.cs
@@ -5,10 +5,58 @@ namespace FinalProject
     public partial class CalculationForm : Form
     {
         private List<Figure> _figures = new List<Figure>();
+        private Color _targetColor = Color.Black;
         public CalculationForm(List<Figure> figures)
         {
             InitializeComponent();
             _figures = figures;
+        }
+
+        private void CalculationForm_Load(object sender, EventArgs e)
+        {
+            lblColor.BackColor = _targetColor;
+
+            var countByFillColor = _figures.Count(figure => figure.FillColor == _targetColor);
+            lblCountSameFillColor.Text = countByFillColor.ToString();
+
+            var countByOutlineColor = _figures.Count(figure => figure.OutlineColor == _targetColor);
+            lblCountSameOutlineColor.Text = countByOutlineColor.ToString();
+
+            var countAllFigures = _figures
+                .Count();
+            lblCountAllFigures.Text = countAllFigures.ToString();
+        }
+
+        private void btnChooseColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            DialogResult result = colorDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                _targetColor = colorDialog.Color;
+                lblColor.BackColor = _targetColor;
+            }
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            lblColor.BackColor = _targetColor;
+
+            var countByFillColor = _figures.Count(figure => figure.FillColor == _targetColor);
+            lblCountSameFillColor.Text = countByFillColor.ToString();
+
+            var countByOutlineColor = _figures.Count(figure => figure.OutlineColor == _targetColor);
+            lblCountSameOutlineColor.Text = countByOutlineColor.ToString();
+
+            var countAllFigures = _figures
+                .Count();
+            lblCountAllFigures.Text = countAllFigures.ToString();
+
         }
     }
 }
